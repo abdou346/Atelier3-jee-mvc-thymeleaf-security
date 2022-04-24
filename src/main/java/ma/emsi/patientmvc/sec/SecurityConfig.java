f7322576bd07d5bda475f33a59eefc54bf76d2df
Abdou346 <aelkhattabi434@gmail.com>
2022-04-24 18:57:24 +0000

    Part4

--- src/main/java/ma/emsi/patientmvc/sec/SecurityConfig.java
+++ src/main/java/ma/emsi/patientmvc/sec/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ma.emsi.patientmvc.sec;
 
+import ma.emsi.patientmvc.sec.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,9 +22,13 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     protected void configure(AuthenticationManagerBuilder auth)throws Exception{
-        PasswordEncoder passwordEncoder=passwordEncoder();
+
         /*PasswordEncoder passwordEncoder=passwordEncoder();
         String encodedPWD=passwordEncoder.encode("1234");
         auth.inMemoryAuthentication()
@@ -30,25 +38,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         auth.inMemoryAuthentication() .withUser("admin").password(passwordEncoder.encode("2546")).roles("USER","ADMIN");
         */
-auth.jdbcAuthentication()
+/*auth.jdbcAuthentication()
         .dataSource(dataSource)
         .usersByUsernameQuery("select username as principal, password as credentials,active from users where username=?")
         .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username=?")
         .rolePrefix("ROLE_")
         .passwordEncoder(passwordEncoder);
+        */
+auth.userDetailsService(userDetailsService);
+
+    /* auth.userDetailsService(new UserDetailsService() {
+         @Override
+         public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+             return null;
+         }
+
+
+    });*/
     }
     @Override
     protected void configure(HttpSecurity http)throws Exception{
         http.formLogin();
         http.authorizeRequests().antMatchers("/").permitAll();
         http.authorizeRequests().antMatchers("/webjars/**").permitAll();
-        http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
-        http.authorizeRequests().antMatchers("/user/**").hasRole("USER");
+        http.authorizeRequests().antMatchers("/admin/**").hasAuthority("ADMIN");
+        http.authorizeRequests().antMatchers("/user/**").hasAuthority("USER");
         http.authorizeRequests().anyRequest().authenticated();
         http.exceptionHandling().accessDeniedPage("/403");
     }
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+
 }
---
+++ src/main/java/ma/emsi/patientmvc/sec/repositories/AppUserRepository.java
@@ -0,0 +1,10 @@
+package ma.emsi.patientmvc.sec.repositories;
+
+import ma.emsi.patientmvc.sec.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository <AppUser,String>{
+    AppUser findByUsername(String username);
+
+
+}
